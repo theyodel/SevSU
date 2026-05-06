ecb56c38d098ff84ca0cedae8d73187bd6fb024c
yodel <skobozabr24@gmail.com>
2026-05-06 17:53:26 +0300

    lab04-AnP

--- 1_Course/2_Term/Algorithmization_and_Programming/LR_4/task.c
+++ 1_Course/2_Term/Algorithmization_and_Programming/LR_4/task.c
@@ -8,10 +8,9 @@ struct scientist {
     int quotes, articles, hirshIndex, id;
 };
 
-
 struct scientist readData();
 void createBinFile();
-int maxHirsh();
+void maxHirsh();
 void readFile();
 void sortInFile();
 
@@ -22,33 +21,30 @@ int main() {
     
     while (1) {
         printf("\n\n\n================ МЕНЮ ================\n");
-        printf("1. Запись файла\n"); //
+        printf("1. Запись файла\n");
         printf("2. Сортировка по полю \"Кол-во цитирований\"\n");
-        printf("3. Вывести учёного с наибольшим индексом Хирша\n"); //
-        printf("4. Вывести таблицу из файла на экран\n"); //
-        printf("0. Выход\n-> "); //
+        printf("3. Вывести учёного с наибольшим индексом Хирша\n");
+        printf("4. Вывести таблицу из файла на экран\n");
+        printf("0. Выход\n-> ");
         scanf("%d", &choice);
+        while (getchar() != '\n'); // очистка буфера после scanf
 
         switch (choice) {
         case 1:
             createBinFile();
             break;
-        
         case 2:
+            sortInFile();
             break;
-        
         case 3:
             maxHirsh();
             break;
-
         case 4:
             readFile();
             break;
-        
         case 0:
             printf("\n\n\nВыход из программы...");
             return 0;
-
         default:
             printf("Команда не распознана!\n");
             break;
@@ -87,26 +83,36 @@ void createBinFile() {
     fflush(stdin);
     fgets(fileName, 100, stdin);
     fileName[strcspn(fileName, "\n")] = '\0';
-    if (fileName[strlen(fileName)-4]!='.' && fileName[strlen(fileName)-3]!='b' && fileName[strlen(fileName)-2]!='i' && fileName[strlen(fileName)-1]!='n') strcat(fileName, ".bin");
+    
+    // Добавляем .bin, если нет расширения
+    if (strlen(fileName) < 4 || strcmp(fileName + strlen(fileName) - 4, ".bin") != 0)
+        strcat(fileName, ".bin");
+    
     FILE *file = fopen(fileName, "wb");
     if (file == NULL) {
-        printf("Файл '%s' будет создан в корне текущей папки.", fileName);
+        printf("Ошибка: не удалось создать файл '%s'!\n", fileName);
+        return;
     }
+    
     int flag = 1;
     struct scientist data;
-    while (flag) {
+    while (1) {
         printf("1. Ввести запись\n");
         printf("0. Сохранить файл\n");
         printf("-> ");
         scanf("%d", &flag);
+        while (getchar() != '\n');
         if (flag == 1) {
             data = readData();
             fwrite(&data, sizeof(struct scientist), 1, file);
-        } else break;
+        } else if (flag == 0) {
+            break;
+        } else {
+            printf("Некорректный ввод! Повторите.\n");
+        }
     }
     printf("Таблица успешно экспортирована в файл '%s'", fileName);
     fclose(file);
-    return;
 }
 
 void readFile() {
@@ -115,7 +121,10 @@ void readFile() {
     fflush(stdin);
     fgets(fileName, 100, stdin);
     fileName[strcspn(fileName, "\n")] = '\0';
-    if (fileName[strlen(fileName)-4]!='.' && fileName[strlen(fileName)-3]!='b' && fileName[strlen(fileName)-2]!='i' && fileName[strlen(fileName)-1]!='n') strcat(fileName, ".bin");
+    
+    if (strlen(fileName) < 4 || strcmp(fileName + strlen(fileName) - 4, ".bin") != 0)
+        strcat(fileName, ".bin");
+    
     FILE *file = fopen(fileName, "rb");
     struct scientist data;
 
@@ -123,81 +132,110 @@ void readFile() {
         printf("Файл '%s' не найден!\n", fileName);
         return;
     }
+    
     printf("+------+-----------------------------------------------------------------------------+---------------------------+---------------------------+---------------+--------------------+--------------+\n");
     printf("|  ID  |                             Фамилия Имя Отчество                            |       Учёная Степень      |        Область Науки      | Кол-во статей | Кол-во цитирований | Индекс Хирша |\n");
     printf("+------+-----------------------------------------------------------------------------+---------------------------+---------------------------+---------------+--------------------+--------------+\n");
+    
     while (fread(&data, sizeof(struct scientist), 1, file) == 1) {
         printf("| %-4d | %-75s | %-25s | %-25s | %-13d | %-18d | %-12d |\n",
-            data.id,
-            data.name,
-            data.degree,
-            data.area,
-            data.articles,
-            data.quotes,
-            data.hirshIndex);
+            data.id, data.name, data.degree, data.area,
+            data.articles, data.quotes, data.hirshIndex);
         printf("+------+-----------------------------------------------------------------------------+---------------------------+---------------------------+---------------+--------------------+--------------+\n");
     }
+    fclose(file);
 }
 
-int maxHirsh() {
+void maxHirsh() {
     char fileName[101];
     printf("Введите название файла -> ");
     fflush(stdin);
     fgets(fileName, 100, stdin);
     fileName[strcspn(fileName, "\n")] = '\0';
-    if (fileName[strlen(fileName)-4]!='.' && fileName[strlen(fileName)-3]!='b' && fileName[strlen(fileName)-2]!='i' && fileName[strlen(fileName)-1]!='n') strcat(fileName, ".bin");
+    
+    if (strlen(fileName) < 4 || strcmp(fileName + strlen(fileName) - 4, ".bin") != 0)
+        strcat(fileName, ".bin");
+    
     FILE *file = fopen(fileName, "rb");
-    struct scientist data, max;
-    int maxHirsh = -1;
-
     if (file == NULL) {
-        printf("Файл '%s' не найден!", fileName);
-        return -1;
+        printf("Файл '%s' не найден!\n", fileName);
+        return;
     }
+    
+    struct scientist data, max = {0};
+    int maxHirshVal = -1;
+    int recordsFound = 0;
 
     while (fread(&data, sizeof(struct scientist), 1, file) == 1) {
-        if (data.hirshIndex > maxHirsh) {
-            maxHirsh = data.hirshIndex;
+        if (data.hirshIndex > maxHirshVal) {
+            maxHirshVal = data.hirshIndex;
             max = data;
+            recordsFound = 1;
         }
     }
+    
+    if (!recordsFound) {
+        printf("Файл '%s' не содержит записей.\n", fileName);
+        fclose(file);
+        return;
+    }
+    
     printf("+------+-----------------------------------------------------------------------------+---------------------------+---------------------------+---------------+--------------------+--------------+\n");
     printf("|  ID  |                             Фамилия Имя Отчество                            |       Учёная Степень      |        Область Науки      | Кол-во статей | Кол-во цитирований | Индекс Хирша |\n");
     printf("+------+-----------------------------------------------------------------------------+---------------------------+---------------------------+---------------+--------------------+--------------+\n");
     printf("| %-4d | %-75s | %-25s | %-25s | %-13d | %-18d | %-12d |\n",
-            max.id,
-            max.name,
-            max.degree,
-            max.area,
-            max.articles,
-            max.quotes,
-            max.hirshIndex);
+            max.id, max.name, max.degree, max.area,
+            max.articles, max.quotes, max.hirshIndex);
     printf("+------+-----------------------------------------------------------------------------+---------------------------+---------------------------+---------------+--------------------+--------------+\n");
-    return maxHirsh;
+    fclose(file);
 }
 
 void sortInFile() {
-    long i,j;
     char fileName[101];
-    struct scientist f, s;
     printf("Введите название файла: ");
+    fflush(stdin);
     fgets(fileName, 100, stdin);
     fileName[strcspn(fileName, "\n")] = '\0';
-    if (fileName[strlen(fileName)-4]!='.' && fileName[strlen(fileName)-3]!='b' && fileName[strlen(fileName)-2]!='i' && fileName[strlen(fileName)-1]!='n') strcat(fileName, ".bin");
-    FILE *file = fopen(fileName, "wb+");
-    long len=ftell(file)/sizeof(struct scientist), elementSize = sizeof(struct scientist);
-    for(i=len-1; i>=1; i--) {
-        for (j=0; j<=i-1; j++) {
-            fseek(file,j*elementSize, SEEK_SET);
-            fread(&f, elementSize, 1, file);
-            fread(&s, elementSize, 1, file);
+    
+    if (strlen(fileName) < 4 || strcmp(fileName + strlen(fileName) - 4, ".bin") != 0)
+        strcat(fileName, ".bin");
+    
+    FILE *file = fopen(fileName, "rb+");
+    if (file == NULL) {
+        printf("Файл '%s' не найден!\n", fileName);
+        return;
+    }
+    
+    // Определяем количество записей
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    long elementSize = sizeof(struct scientist);
+    long len = fileSize / elementSize;
+    
+    if (len <= 1) {
+        printf("Файл пуст или содержит только одну запись. Сортировка не нужна.\n");
+        fclose(file);
+        return;
+    }
+    
+    // Пузырьковая сортировка
+    struct scientist f, s;
+    for (long i = len - 1; i >= 1; i--) {
+        for (long j = 0; j <= i - 1; j++) {
+            fseek(file, j * elementSize, SEEK_SET);
+            if (fread(&f, elementSize, 1, file) != 1 || fread(&s, elementSize, 1, file) != 1) {
+                printf("Ошибка чтения из файла.\n");
+                fclose(file);
+                return;
+            }
             if (f.quotes > s.quotes) {
-                fseek(file, (-2)*elementSize, SEEK_CUR);
+                fseek(file, j * elementSize, SEEK_SET);
                 fwrite(&s, elementSize, 1, file);
                 fwrite(&f, elementSize, 1, file);
             }
         }
     }
-    printf("Сортировка успешна завершена");
-    return;
+    
+    printf("Сортировка успешно завершена\n");
+    fclose(file);
 }
