45730a767362600bf0d96be31d3de945deb6a43e
yodel <skobozabr24@gmail.com>
2026-09-08 11:19:01 +0300

    lab01-oop started

---
+++ 2_Course/3_Term/Object‑Oriented_Programming/lab_01/task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+struct man {
+    string name, surname, sex;
+    int age, num, height;
+    double weight;
+};
+
+int main() {
+    cout << "тут код";
+    return 0;
+}
---
+++ 2_Course/3_Term/Object‑Oriented_Programming/lab_01/task2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+class man {
+    private:
+        string name, surname, sex;
+        int age, num, height;
+        double weight;
+    
+    public:
+        // прототипы
+};
+
+int main() {
+    // код
+    return 0;
+}
